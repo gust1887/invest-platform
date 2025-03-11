92b7c458ccbbd74d3fd2f00214d97fc79e2a67eb
gust1887 <gustavman167@gmail.com>
2025-03-11 10:42:44 +0100

    Test for at øve

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server kører på http://localhost:${port}`);
+});
