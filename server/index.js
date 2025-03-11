fffcb6a589ff589f8c5cadf4773f4e82b6195824
gust1887 <gustavman167@gmail.com>
2025-03-11 13:42:22 +0100

    Så er der sat en server op samt. paths til de forskellige sider

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 5000;
+
+
+app.use(express.static(path.join(__dirname, '..', 'HTML')));  // HTML mappen ligger uden for server mappen
+
+// Hjemmeside rute (index.html)
+app.get('/', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '..', 'HTML', 'frontpage.html'));  // Path til frontpage.html
+});
+
+
+
+
+// Start serveren
+app.listen(port, () => {
+  console.log(`Server kører på http://localhost:${port}`);
+});
